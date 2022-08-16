c5202995c8c3dfef85cf2fed14954a76302ac65c
john <johnzhengyuliu@gmail.com>
2022-08-16 16:17:36 -0500

    draft

--- programs/student-intro/src/lib.rs
+++ programs/student-intro/src/lib.rs
@@ -20,11 +20,6 @@ pub mod student_intro {
         student_intro.name = name;
         student_intro.message = message;
 
-        msg!("Reply Counter Account Created");
-        let reply_counter = &mut ctx.accounts.reply_counter;
-        reply_counter.counter = 0;
-        msg!("Counter: {}", reply_counter.counter);
-
         Ok(())
     }
 
@@ -45,19 +40,7 @@ pub mod student_intro {
         Ok(())
     }
 
-    pub fn add_reply(ctx: Context<AddReply>, reply: String) -> Result<()> {
-        msg!("Reply Account Created");
-        msg!("Reply: {}", reply);
-
-        let reply_account = &mut ctx.accounts.reply_account;
-        let reply_account_counter = &mut ctx.accounts.reply_counter;
-
-        reply_account.studentinfo = ctx.accounts.student.key();
-        reply_account.reply = reply;
-        reply_account.count = reply_account_counter.counter;
-
-        reply_account_counter.counter += 1;
-
+    pub fn close(_ctx: Context<Close>) -> Result<()> {
         Ok(())
     }
 }
@@ -73,14 +56,6 @@ pub struct AddStudentIntro<'info> {
         space = 8 + 32 + 4 + name.len() + 4 + message.len()
     )]
     pub student_intro: Account<'info, StudentInfo>,
-    #[account(
-        init,
-        seeds = ["counter".as_bytes(), student_intro.key().as_ref()],
-        bump,
-        payer = student,
-        space = 8 + 8
-    )]
-    pub reply_counter: Account<'info, ReplyCounter>,
     #[account(mut)]
     pub student: Signer<'info>,
     pub system_program: Program<'info, System>,
@@ -104,32 +79,11 @@ pub struct UpdateStudentIntro<'info> {
 }
 
 #[derive(Accounts)]
-#[instruction(reply:String)]
-pub struct AddReply<'info> {
-    #[account(
-        init,
-        seeds = [student_intro.key().as_ref(), &reply_counter.counter.to_le_bytes()],
-        bump,
-        payer = replier,
-        space = 8 + 32 + 4 + reply.len() + 8
-    )]
-    pub reply_account: Account<'info, Reply>,
-    #[account(
-        seeds = [student.key().as_ref()],
-        bump,
-    )]
-    pub student_intro: Account<'info, StudentInfo>,
-    #[account(
-        mut,
-        seeds = ["counter".as_bytes(), student_intro.key().as_ref()],
-        bump,
-    )]
-    pub reply_counter: Account<'info, ReplyCounter>,
+pub struct Close<'info> {
+    #[account(mut, close = user)]
+    student_intro: Account<'info, StudentInfo>,
     #[account(mut)]
-    pub replier: Signer<'info>,
-    /// CHECK:
-    pub student: AccountInfo<'info>,
-    pub system_program: Program<'info, System>,
+    user: Signer<'info>,
 }
 
 #[account]
@@ -137,16 +91,4 @@ pub struct StudentInfo {
     pub student: Pubkey, // 32
     pub name: String,    // 4 + len()
     pub message: String, // 4 + len()
-}
-
-#[account]
-pub struct ReplyCounter {
-    pub counter: u64,
-}
-
-#[account]
-pub struct Reply {
-    pub studentinfo: Pubkey, // 32
-    pub reply: String,       // 4 + len()
-    pub count: u64,          // 8
 }
--- tests/student-intro.ts
+++ tests/student-intro.ts
@@ -16,21 +16,14 @@ describe("student-intro", () => {
       program.programId
     )
 
-    const [replyCounterPda] = await anchor.web3.PublicKey.findProgramAddress(
-      [Buffer.from("counter"), studentIntroPda.toBuffer()],
-      program.programId
-    )
-
     const tx = await program.methods
       .addStudentIntro("name", "message")
       .accounts({
         studentIntro: studentIntroPda,
-        replyCounter: replyCounterPda,
         student: userWallet.publicKey,
         systemProgram: anchor.web3.SystemProgram.programId,
       })
       .rpc()
-    console.log("Your transaction signature", tx)
 
     const studenIntroAccount = await program.account.studentInfo.fetch(
       studentIntroPda
@@ -51,44 +44,12 @@ describe("student-intro", () => {
     )
     console.log(studenIntroAccountRealloc)
 
-    const [replyPda] = await anchor.web3.PublicKey.findProgramAddress(
-      [studentIntroPda.toBuffer(), new BN(0).toArrayLike(Buffer, "le", 8)],
-      program.programId
-    )
-
     const tx3 = await program.methods
-      .addReply("reply")
+      .close()
       .accounts({
-        replyAccount: replyPda,
         studentIntro: studentIntroPda,
-        replyCounter: replyCounterPda,
-        replier: userWallet.publicKey,
-        student: userWallet.publicKey,
-        systemProgram: anchor.web3.SystemProgram.programId,
+        user: userWallet.publicKey,
       })
       .rpc()
-
-    const replyAccount = await program.account.reply.fetch(replyPda)
-    console.log(replyAccount)
-
-    const [replyPda2] = await anchor.web3.PublicKey.findProgramAddress(
-      [studentIntroPda.toBuffer(), new BN(1).toArrayLike(Buffer, "le", 8)],
-      program.programId
-    )
-
-    const tx4 = await program.methods
-      .addReply("reply2")
-      .accounts({
-        replyAccount: replyPda2,
-        studentIntro: studentIntroPda,
-        replyCounter: replyCounterPda,
-        replier: userWallet.publicKey,
-        student: userWallet.publicKey,
-        systemProgram: anchor.web3.SystemProgram.programId,
-      })
-      .rpc()
-
-    const replyAccount2 = await program.account.reply.fetch(replyPda2)
-    console.log(replyAccount2)
   })
 })
